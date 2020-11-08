7fa85c606f63a59b00ca25be66658bead7db6663
powerdigital-dev <johnny1864@yahoo.com>
2020-11-07 19:43:05 -0800

    updated header

---
+++ js/main.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(($) => {
+    $('.projects--slider').slick({
+      mobileFirst: true,
+      slidesToShow: 1,
+      infinite: true,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            centerMode: true,
+          },
+        },
+      ],
+    });
+
+
+    $('.burger-menu').click(function () {
+      $(this).toggleClass('active');
+    });
+
+    $(window).on('scroll', function(){
+       
+        //Adds class 'shrink' to nav on scroll
+        if($(window).scrollTop()>70){
+            $('header').addClass('scroll');
+        }else{
+            $('header').removeClass('scroll');
+        }
+    });
+  });
